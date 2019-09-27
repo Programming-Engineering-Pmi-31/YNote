3d37dd2afeaf0dec1c51661ffad630561937b765
f_o_x_l_v_i_v <burco.ab@gmail.com>
2019-09-27 22:34:33 +0300

    Create configurations for entities

---
+++ YNote.Infrastructure/Configurations/NoteConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YNote.Domain.Entities;
+
+namespace YNote.Infrastructure.Configurations
+{
+    class NoteConfiguration : IEntityTypeConfiguration<NoteEntity>
+    {
+        public void Configure(EntityTypeBuilder<NoteEntity> builder)
+        {
+            builder.ToTable("Notes");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.CreationTime).IsRequired();
+
+            builder.HasOne(e => e.Space)
+                .WithMany(space => space.Notes)
+                .HasForeignKey(e => e.SpaceId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(e => e.Group)
+                .WithMany(group => group.Notes)
+                .HasForeignKey(e => e.GroupId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(e => e.AssignedUser)
+                .WithMany(user => user.Notes)
+                .HasForeignKey(e => e.AssignedUserId)
+                .OnDelete(DeleteBehavior.SetNull);
+                
+        }
+    }
+}
---
+++ YNote.Infrastructure/Configurations/SpaceConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YNote.Domain.Entities;
+
+namespace YNote.Infrastructure.Configurations
+{
+    class SpaceConfiguration : IEntityTypeConfiguration<SpaceEntity>
+    {
+        public void Configure(EntityTypeBuilder<SpaceEntity> builder)
+        {
+            builder.ToTable("Spaces");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.SpaceName).IsRequired();
+
+            builder.Property(e => e.AuthorId).IsRequired();
+
+            builder.Property(e => e.SpaceName).HasMaxLength(100);
+
+            builder.HasOne(e => e.Author)
+                .WithMany(author => author.Spaces)
+                .HasForeignKey(e => e.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
