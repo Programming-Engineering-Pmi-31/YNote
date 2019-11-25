98849339a3d823b44d618b7b550adfa20d680125
f_o_x_l_v_i_v <burco.ab@gmail.com>
2019-11-25 15:32:11 +0200

    Add services with Get

---
+++ YNoteWPF.BLL/Data/GroupService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using YNoteWPF.DAL;
+using YNoteWPF.DAL.Entities;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace YNoteWPF.BLL.Data
+{
+    public class GroupService : IGroupService
+    {
+        private Func<YNoteDbContext> _contextCreator;
+
+        public GroupService(Func<YNoteDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
+
+        public async Task<List<GroupEntity>> GetAllAsync()
+        {
+            using (var ctx = _contextCreator())
+            {
+                return await ctx.Groups.AsNoTracking().ToListAsync();
+            }
+        }
+
+        public Task<GroupEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YNoteWPF.BLL/Data/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YNoteWPF.DAL.Entities;
+
+namespace YNoteWPF.BLL.Data
+{
+    public interface INoteService
+    {
+        Task<List<NoteEntity>> GetAllAsync();
+
+        Task<NoteEntity> GetByIdAsync(int id);
+    }
+}
