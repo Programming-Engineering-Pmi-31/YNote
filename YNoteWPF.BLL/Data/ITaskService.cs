4e04a4446adc5dfd639a1945c2db9e51c969b1ae
Nazar <nazar1124@gmail.com>
2019-12-02 12:34:24 +0200

    Made registration. But have to finish validation tests.There are some errors

---
+++ YNoteWPF.BLL/Data/ITaskService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YNoteWPF.DAL.Entities;
+
+namespace YNoteWPF.BLL.Data
+{
+    public interface ITaskService
+    {
+        Task<List<TaskEntity>> GetAllAsync();
+
+        Task<TaskEntity> GetByIdAsync(int id);
+    }
+}
---
+++ YNoteWPF.BLL/Data/SpaceService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using YNoteWPF.DAL;
+using YNoteWPF.DAL.Entities;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace YNoteWPF.BLL.Data
+{
+    public class SpaceService : ISpaceService
+    {
+        private Func<YNoteDbContext> _contextCreator;
+
+        public SpaceService(Func<YNoteDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
+
+        public async Task<List<SpaceEntity>> GetAllAsync()
+        {
+            using (var ctx = _contextCreator())
+            {
+                return await ctx.Spaces.AsNoTracking().ToListAsync();
+            }
+        }
+
+        public Task<SpaceEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
