6b9dc984f46b2ab753f86d9613ddf6d4dffaeff0
f_o_x_l_v_i_v <burco.ab@gmail.com>
2019-12-08 23:56:54 +0200

    Add AutoMapper package

---
+++ YNoteWPF.BLL/Mappers/ServicesMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using YNoteWPF.BLL.Data.Models;
+using YNoteWPF.DAL.Entities;
+
+namespace YNoteWPF.BLL.Mappers
+{
+    class ServicesMapperProfile: Profile
+    {
+        public ServicesMapperProfile()
+        {
+            CreateMap<CreateTaskDTO, TaskEntity>();
+            CreateMap<TaskEntity, TaskDTO>();
+            CreateMap<UpdateTaskDTO, TaskEntity>();
+            CreateMap<UpdateTaskDTO, CreateTaskDTO>();
+        }
+    }
+}
