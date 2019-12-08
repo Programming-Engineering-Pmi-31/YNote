016a3d42ea17ea3b492f7cf529ddcf869424cf4e
f_o_x_l_v_i_v <burco.ab@gmail.com>
2019-12-08 23:57:49 +0200

    Add create and update DTOs for Note and task

---
+++ YNoteWPF.BLL/Data.Models/CreateNoteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YNoteWPF.BLL.Data.Models
+{
+    public class CreateNoteDTO
+    {
+
+        public SpaceDTO Space { get; set; }
+
+        public DateTimeOffset CreationTime { get; set; }
+
+        public List<CreateTaskDTO> Tasks { get; set; }
+    }
+}
---
+++ YNoteWPF.BLL/Data.Models/UpdateTaskDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace YNoteWPF.BLL.Data.Models
+{
+    public class UpdateTaskDTO
+    {
+        public int Id { get; set; }
+
+        public string SumUp { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
